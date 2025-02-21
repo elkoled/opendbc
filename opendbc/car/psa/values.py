85551c3d2168c9e9f535eccf413c5d8430397f9d
elkoled <elkoled@gmail.com>
2025-02-21 11:57:59 +0100

    reduce angle rate limits

--- opendbc/car/psa/values.py
+++ opendbc/car/psa/values.py
@@ -10,8 +10,8 @@ Ecu = CarParams.Ecu
 class CarControllerParams:
   STEER_STEP = 1  # spamming at 100 Hz works well, stock lkas is ~20 Hz
   STEER_MAX = 390.0  # EPS can actuate the full range of steering
-  ANGLE_RATE_LIMIT_UP = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[4.0, 0.64, 0.12])
-  ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[4.0, 2.8, 0.32])
+  ANGLE_RATE_LIMIT_UP = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[4., .5, .1]) # TODO: refine limits
+  ANGLE_RATE_LIMIT_DOWN = AngleRateLimit(speed_bp=[0., 5., 15.], angle_v=[4., 2.5, .3]) # TODO: refine limits
   STEER_DRIVER_ALLOWANCE = 10  # Driver intervention threshold, 1 Nm
 
   def __init__(self, CP):
