42615552af6741be7ae07d6076ca8b57d45f965c
elkoled <elkoled@gmail.com>
2025-03-13 18:16:24 +0100

    DEBUG: disable radar

--- opendbc/car/disable_ecu.py
+++ opendbc/car/disable_ecu.py
@@ -1,5 +1,8 @@
 from opendbc.car.carlog import carlog
 from opendbc.car.isotp_parallel_query import IsoTpParallelQuery
+from panda import Panda
+from opendbc.car import uds
+import time
 
 EXT_DIAG_REQUEST = b'\x10\x03'
 EXT_DIAG_RESPONSE = b'\x50\x03'
@@ -13,20 +16,16 @@ def disable_ecu(can_recv, can_send, bus=0, addr=0x7d0, sub_addr=None, com_cont_r
 
   This is used to disable the radar in some cars. Openpilot will emulate the radar.
   WARNING: THIS DISABLES AEB!"""
+  time.sleep(120)
   carlog.warning(f"ecu disable {hex(addr), sub_addr} ...")
 
   for i in range(retry):
     try:
-      query = IsoTpParallelQuery(can_send, can_recv, bus, [(addr, sub_addr)], [EXT_DIAG_REQUEST], [EXT_DIAG_RESPONSE])
-
-      for _, _ in query.get_data(timeout).items():
-        carlog.warning("communication control disable tx/rx ...")
-
-        query = IsoTpParallelQuery(can_send, can_recv, bus, [(addr, sub_addr)], [com_cont_req], [COM_CONT_RESPONSE])
-        query.get_data(0)
-
-        carlog.warning("ecu disabled")
-        return True
+      panda = Panda()
+      radar_uds = uds.UdsClient(panda, tx_addr=addr, rx_addr=696, bus=1)
+      radar_uds.diagnostic_session_control(uds.SESSION_TYPE.PROGRAMMING)
+      carlog.error("ecu disabled")
+      return True
 
     except Exception:
       carlog.exception("ecu disable exception")
