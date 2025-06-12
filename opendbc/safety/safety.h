bad83df831348c0812fbf891383eb7cd82c91be5
elkoled <elkoled@gmail.com>
2025-06-12 23:16:37 +0200

    add psa safety hook

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -415,6 +415,7 @@ int set_safety_hooks(uint16_t mode, uint16_t param) {
     {SAFETY_FORD, &ford_hooks},
     {SAFETY_RIVIAN, &rivian_hooks},
     {SAFETY_TESLA, &tesla_hooks},
+    {SAFETY_PSA, &psa_hooks},
 #ifdef CANFD
     {SAFETY_HYUNDAI_CANFD, &hyundai_canfd_hooks},
 #endif
