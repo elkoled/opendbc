65f3d0befd6dc923cac6c8e213d406ba0b6f97e3
elkoled <elkoled@gmail.com>
2025-07-02 19:44:01 +0200

    DEBUG: add torque blending

--- opendbc/car/psa/carcontroller.py
+++ opendbc/car/psa/carcontroller.py
@@ -3,12 +3,24 @@ from opendbc.can.packer import CANPacker
 from opendbc.car.interfaces import CarControllerBase
 from opendbc.car.psa.psacan import create_lka_steering
 from opendbc.car.psa.values import CarControllerParams
+import numpy as np
+
+TORQUE_DEADZONE = 0.5      # Ignore tiny torques (hands-off)
+TORQUE_GAIN = 4.0          # How much driver torque affects steering angle
+TORQUE_CLIP = 10.0         # Safety clamp
 
 class CarController(CarControllerBase):
   def __init__(self, dbc_names, CP, CP_SP):
     super().__init__(dbc_names, CP, CP_SP)
     self.packer = CANPacker(dbc_names[Bus.cam])
     self.apply_angle_last = 0
+
+  def blend_angle(self, apply_angle, driver_torque):
+    if abs(driver_torque) < TORQUE_DEADZONE:
+      return apply_angle
+    # Adjust angle in direction of driver torque
+    delta = np.clip(driver_torque, -TORQUE_CLIP, TORQUE_CLIP) * TORQUE_GAIN
+    return apply_angle + delta
 
   def update(self, CC, CC_SP, CS, now_nanos):
     can_sends = []
@@ -18,6 +30,8 @@ class CarController(CarControllerBase):
     if CC.latActive:
       apply_angle = apply_std_steer_angle_limits(actuators.steeringAngleDeg, self.apply_angle_last, CS.out.vEgoRaw,
                                                    CS.out.steeringAngleDeg, CC.latActive, CarControllerParams.ANGLE_LIMITS)
+      # Blend driver torque
+      apply_angle = self.blend_angle(apply_angle, CS.out.steeringTorque)
     else:
       apply_angle = 0
 
