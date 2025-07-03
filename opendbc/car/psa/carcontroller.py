855632fae0712f176c2fced672666171536ac345
elkoled <elkoled@gmail.com>
2025-07-03 13:30:47 +0200

    DEBUG: fix torque blending

--- opendbc/car/psa/carcontroller.py
+++ opendbc/car/psa/carcontroller.py
@@ -5,9 +5,11 @@ from opendbc.car.psa.psacan import create_lka_steering
 from opendbc.car.psa.values import CarControllerParams
 import numpy as np
 
-TORQUE_DEADZONE = 0.5      # Ignore tiny torques (hands-off)
-TORQUE_GAIN = 4.0          # How much driver torque affects steering angle
-TORQUE_CLIP = 10.0         # Safety clamp
+# Torque blending parameters
+TORQUE_TO_ANGLE_MULTIPLIER_OUTER = 4  # Higher = easier to influence when manually steering more than OP
+TORQUE_TO_ANGLE_MULTIPLIER_INNER = 8  # Higher = easier to influence when manually steering less than OP
+TORQUE_TO_ANGLE_DEADZONE = 5  # 0.5 Nm
+TORQUE_TO_ANGLE_CLIP = 50  # 5 Nm
 
 class CarController(CarControllerBase):
   def __init__(self, dbc_names, CP, CP_SP):
@@ -15,12 +17,21 @@ class CarController(CarControllerBase):
     self.packer = CANPacker(dbc_names[Bus.cam])
     self.apply_angle_last = 0
 
-  def blend_angle(self, apply_angle, driver_torque):
-    if abs(driver_torque) < TORQUE_DEADZONE:
+  def torque_blended_angle(self, apply_angle, driver_torque):
+    deadzone = TORQUE_TO_ANGLE_DEADZONE
+    if abs(driver_torque) < deadzone:
       return apply_angle
-    # Adjust angle in direction of driver torque
-    delta = np.clip(driver_torque, -TORQUE_CLIP, TORQUE_CLIP) * TORQUE_GAIN
-    return apply_angle + delta
+
+    limit = TORQUE_TO_ANGLE_CLIP
+    if apply_angle * driver_torque >= 0:
+      # user override in the same direction
+      strength = TORQUE_TO_ANGLE_MULTIPLIER_OUTER
+    else:
+      # user override in the opposite direction
+      strength = TORQUE_TO_ANGLE_MULTIPLIER_INNER
+
+    torque = driver_torque - deadzone if driver_torque > 0 else driver_torque + deadzone
+    return apply_angle + np.clip(torque, -limit, limit) * strength
 
   def update(self, CC, CC_SP, CS, now_nanos):
     can_sends = []
@@ -30,14 +41,15 @@ class CarController(CarControllerBase):
     if CC.latActive:
       apply_angle = apply_std_steer_angle_limits(actuators.steeringAngleDeg, self.apply_angle_last, CS.out.vEgoRaw,
                                                    CS.out.steeringAngleDeg, CC.latActive, CarControllerParams.ANGLE_LIMITS)
-      # Blend driver torque
-      apply_angle = self.blend_angle(apply_angle, CS.out.steeringTorque)
+
+      # torque blending
+      apply_angle = self.torque_blended_angle(apply_angle, CS.out.steeringTorque)
     else:
       apply_angle = 0
 
     can_sends.append(create_lka_steering(self.packer, self.frame // 5, CC.latActive, apply_angle))
 
-    self.apply_angle_last = apply_angle
+    self.apply_angle_last = float(apply_angle)
 
     new_actuators = actuators.as_builder()
     new_actuators.steeringAngleDeg = self.apply_angle_last
