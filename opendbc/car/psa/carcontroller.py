1c58d1f1c3c4335defc30e0c7f87600bbbc9cd1c
elkoled <elkoled@gmail.com>
2025-07-18 00:32:39 +0200

    restore carcontroller

--- opendbc/car/psa/carcontroller.py
+++ opendbc/car/psa/carcontroller.py
@@ -1,75 +1,23 @@
-import math
-import numpy as np
-from opendbc.car import ACCELERATION_DUE_TO_GRAVITY, Bus, AngleSteeringLimits, DT_CTRL, rate_limit
+from opendbc.car import apply_std_steer_angle_limits, Bus
 from opendbc.can.packer import CANPacker
-from opendbc.car.interfaces import CarControllerBase, ISO_LATERAL_ACCEL
+from opendbc.car.interfaces import CarControllerBase
 from opendbc.car.psa.psacan import create_lka_steering
 from opendbc.car.psa.values import CarControllerParams
-from opendbc.car.vehicle_model import VehicleModel
-
-# Add extra tolerance for average banked road since safety doesn't have the roll
-AVERAGE_ROAD_ROLL = 0.06  # ~3.4 degrees, 6% superelevation. higher actual roll lowers lateral acceleration
-MAX_LATERAL_ACCEL = ISO_LATERAL_ACCEL + (ACCELERATION_DUE_TO_GRAVITY * AVERAGE_ROAD_ROLL)  # ~3.6 m/s^2
-MAX_LATERAL_JERK = 3.0 + (ACCELERATION_DUE_TO_GRAVITY * AVERAGE_ROAD_ROLL)  # ~3.6 m/s^3
-
-
-def get_max_angle_delta(v_ego_raw: float, VM: VehicleModel):
-  max_curvature_rate_sec = MAX_LATERAL_JERK / (v_ego_raw ** 2)  # (1/m)/s
-  max_angle_rate_sec = math.degrees(VM.get_steer_from_curvature(max_curvature_rate_sec, v_ego_raw, 0))  # deg/s
-  return max_angle_rate_sec * (DT_CTRL * 1)
-
-
-def get_max_angle(v_ego_raw: float, VM: VehicleModel):
-  max_curvature = MAX_LATERAL_ACCEL / (v_ego_raw ** 2)  # 1/m
-  return math.degrees(VM.get_steer_from_curvature(max_curvature, v_ego_raw, 0))  # deg
-
-
-def apply_psa_steer_angle_limits(apply_angle: float, apply_angle_last: float, v_ego_raw: float, steering_angle: float,
-                                 lat_active: bool, limits: AngleSteeringLimits, VM: VehicleModel) -> float:
-  v_ego_raw = max(v_ego_raw, 1)
-
-  # *** max lateral jerk limit ***
-  max_angle_delta = get_max_angle_delta(v_ego_raw, VM)
-
-  # use PSA angle limits
-  new_apply_angle = rate_limit(apply_angle, apply_angle_last, -max_angle_delta, max_angle_delta)
-
-  # *** max lateral accel limit ***
-  max_angle = get_max_angle(v_ego_raw, VM)
-  new_apply_angle = np.clip(new_apply_angle, -max_angle, max_angle)
-
-  # angle is current angle when inactive
-  if not lat_active:
-    new_apply_angle = steering_angle
-
-  # prevent fault
-  return float(np.clip(new_apply_angle, -limits.STEER_ANGLE_MAX, limits.STEER_ANGLE_MAX))
-
-
-def get_safety_CP():
-  # Use PSA platform for lateral limiting to match safety
-  from opendbc.car.psa.interface import CarInterface
-  return CarInterface.get_non_essential_params("PSA_PEUGEOT_208")
-
 
 class CarController(CarControllerBase):
-  def __init__(self, dbc_names, CP, CP_SP):
-    super().__init__(dbc_names, CP, CP_SP)
+  def __init__(self, dbc_names, CP):
+    super().__init__(dbc_names, CP)
     self.packer = CANPacker(dbc_names[Bus.cam])
     self.apply_angle_last = 0
 
-    # Vehicle model used for lateral limiting
-    self.VM = VehicleModel(get_safety_CP())
-
-  def update(self, CC, CC_SP, CS, now_nanos):
+  def update(self, CC, CS, now_nanos):
     can_sends = []
     actuators = CC.actuators
 
     # lateral control
     if CC.latActive:
-      apply_angle = apply_psa_steer_angle_limits(actuators.steeringAngleDeg, self.apply_angle_last, CS.out.vEgoRaw,
-                                                 CS.out.steeringAngleDeg, CC.latActive,
-                                                 CarControllerParams.ANGLE_LIMITS, self.VM)
+      apply_angle = apply_std_steer_angle_limits(actuators.steeringAngleDeg, self.apply_angle_last, CS.out.vEgoRaw,
+                                                   CS.out.steeringAngleDeg, CC.latActive, CarControllerParams.ANGLE_LIMITS)
     else:
       apply_angle = 0
 
