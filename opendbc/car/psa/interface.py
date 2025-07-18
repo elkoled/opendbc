2a157a9945613cfe1261f65c6b79871fdd0801dc
elkoled <elkoled@gmail.com>
2025-07-18 13:17:17 +0200

    force DashcamMode

--- opendbc/car/psa/interface.py
+++ opendbc/car/psa/interface.py
@@ -12,7 +12,7 @@ class CarInterface(CarInterfaceBase):
   @staticmethod
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams:
     ret.brand = 'psa'
-    ret.dashcamOnly = False
+    ret.dashcamOnly = True
     ret.steerAtStandstill = True
 
     ret.steerControlType = structs.CarParams.SteerControlType.angle
