5bc47774502af5ee5bd0f3f2ae920cd8984a17ab
elkoled <elkoled@gmail.com>
2025-07-21 09:16:08 +0200

    cruisestate available

--- opendbc/car/psa/carstate.py
+++ opendbc/car/psa/carstate.py
@@ -51,7 +51,7 @@ class CarState(CarStateBase):
     # cruise
     ret.cruiseState.speed = cp_adas.vl['HS2_DAT_MDD_CMD_452']['SPEED_SETPOINT'] * CV.KPH_TO_MS # set to 255 when ACC is off, -2 kph offset from dashboard speed
     ret.cruiseState.enabled = cp_adas.vl['HS2_DAT_MDD_CMD_452']['RVV_ACC_ACTIVATION_REQ'] == 1
-    ret.cruiseState.available = cp_adas.vl['HS2_DYN1_MDD_ETAT_2B6']['ACC_STATUS'] > 2
+    ret.cruiseState.available = True#cp_adas.vl['HS2_DYN1_MDD_ETAT_2B6']['ACC_STATUS'] > 2
     ret.cruiseState.nonAdaptive = cp_adas.vl['HS2_DAT_MDD_CMD_452']['LONGITUDINAL_REGULATION_TYPE'] != 3 # 0: None, 1: CC, 2: Limiter, 3: ACC
     ret.cruiseState.standstill = bool(cp_adas.vl['HS2_DYN_UCF_MDD_32D']['VEHICLE_STANDSTILL'])
     ret.accFaulted = cp_adas.vl['HS2_DYN_UCF_MDD_32D']['ACC_ETAT_DECEL_OR_ESP_STATUS'] == 3 # 0: Inhibited, 1: Waiting, 2: Active, 3: Fault
@@ -94,7 +94,7 @@ class CarState(CarStateBase):
       ('HS2_DYN_ABR_38D', 25),
       ('HS2_DYN_UCF_MDD_32D', 50),
       ('HS2_DAT_MDD_CMD_452', 20),
-      ('HS2_DYN1_MDD_ETAT_2B6', 50),
+      # ('HS2_DYN1_MDD_ETAT_2B6', 50),
       # ('HS2_DYN_MDD_ETAT_2F6', 50),
     ]
     main_messages = [
