041489446b577f656640be5763bf6253f80566a4
elkoled <elkoled@gmail.com>
2025-07-21 12:16:27 +0200

    disable tx hook

--- opendbc/safety/safety/safety_psa.h
+++ opendbc/safety/safety/safety_psa.h
@@ -76,25 +76,8 @@ static void psa_rx_hook(const CANPacket_t *to_push) {
 }
 
 static bool psa_tx_hook(const CANPacket_t *to_send) {
-  bool tx = true;
-  int addr = GET_ADDR(to_send);
-
-  // TODO: Safety check for cruise buttons
-  // TODO: check resume is not pressed when controls not allowed
-  // TODO: check cancel is not pressed when cruise isn't engaged
-
-  // Safety check for LKA
-  if (addr == PSA_LANE_KEEP_ASSIST) {
-    // SET_ANGLE
-    int desired_angle = to_signed((GET_BYTE(to_send, 6) << 6) | ((GET_BYTE(to_send, 7) & 0xFCU) >> 2), 14);
-    // TORQUE_FACTOR
-    bool lka_active = ((GET_BYTE(to_send, 5) & 0xFEU) >> 1) == 100U;
-
-    if (steer_angle_cmd_checks(desired_angle, lka_active, PSA_STEERING_LIMITS)) {
-      tx = false;
-    }
-  }
-  return tx;
+  UNUSED(to_send);
+  return result;
 }
 
 static bool psa_fwd_hook(int bus_num, int addr) {
