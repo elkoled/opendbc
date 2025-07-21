6ede1466772cabc48351f754b27a2523b62bd0a0
elkoled <elkoled@gmail.com>
2025-07-21 12:41:44 +0200

    sunnypilot carstate

--- opendbc/car/psa/carstate.py
+++ opendbc/car/psa/carstate.py
@@ -40,18 +40,17 @@ class CarState(CarStateBase):
 
     # steering wheel
     ret.steeringAngleDeg = cp.vl['STEERING_ALT']['ANGLE'] # EPS
-    ret.steeringRateDeg = cp.vl['STEERING_ALT']['RATE'] * (2 * cp.vl['STEERING_ALT']['RATE_SIGN'] - 1) # convert [0,1] to [-1,1] EPS: Rotation speed * rotation sign
+    ret.steeringRateDeg = cp.vl['STEERING_ALT']['RATE'] * (2 * cp.vl['STEERING_ALT']['RATE_SIGN'] - 1) # convert [0,1] to [-1,1] EPS: rot. speed * rot. sign
     ret.steeringTorque = cp.vl['STEERING']['DRIVER_TORQUE']
     ret.steeringTorqueEps = cp.vl['IS_DAT_DIRA']['EPS_TORQUE']
     ret.steeringPressed = self.update_steering_pressed(abs(ret.steeringTorque) > CarControllerParams.STEER_DRIVER_ALLOWANCE, 5)
-    ret.steerFaultTemporary = False # TODO: test  bool(cp.vl['IS_DAT_DIRA']['TRQ_LIMIT_STATE']) # TRQ_LIMIT_STATE triggers before EPS actually gives up
     ret.steerFaultPermanent = bool(cp.vl['IS_DAT_DIRA']['STEERING_REBOOT_REQUEST'])
     ret.espDisabled = bool(cp_adas.vl['ESP']['ESP_STATUS_INV'])
 
     # cruise
-    ret.cruiseState.speed = cp_adas.vl['HS2_DAT_MDD_CMD_452']['SPEED_SETPOINT'] * CV.KPH_TO_MS # set to 255 when ACC is off, -2 kph offset from dashboard speed
+    ret.cruiseState.speed = cp_adas.vl['HS2_DAT_MDD_CMD_452']['SPEED_SETPOINT'] * CV.KPH_TO_MS # set to 255 when ACC is off, -2 kph offset from dash speed
     ret.cruiseState.enabled = cp_adas.vl['HS2_DAT_MDD_CMD_452']['RVV_ACC_ACTIVATION_REQ'] == 1
-    ret.cruiseState.available = True#cp_adas.vl['HS2_DYN1_MDD_ETAT_2B6']['ACC_STATUS'] > 2
+    ret.cruiseState.available = cp_adas.vl['HS2_DYN1_MDD_ETAT_2B6']['ACC_STATUS'] > 2
     ret.cruiseState.nonAdaptive = cp_adas.vl['HS2_DAT_MDD_CMD_452']['LONGITUDINAL_REGULATION_TYPE'] != 3 # 0: None, 1: CC, 2: Limiter, 3: ACC
     ret.cruiseState.standstill = bool(cp_adas.vl['HS2_DYN_UCF_MDD_32D']['VEHICLE_STANDSTILL'])
     ret.accFaulted = cp_adas.vl['HS2_DYN_UCF_MDD_32D']['ACC_ETAT_DECEL_OR_ESP_STATUS'] == 3 # 0: Inhibited, 1: Waiting, 2: Active, 3: Fault
@@ -59,21 +58,18 @@ class CarState(CarStateBase):
     # gear
     if self.CP.transmissionType == TransmissionType.manual:
       ret.clutchPressed = bool(cp.vl['Dyn2_CMM']['P091_ConvCD_stDebVal'])
-    # if bool(cp_main.vl['Dat_BSI']['P103_Com_bRevGear']):
-    #   ret.gearShifter = GearShifter.reverse
+    if bool(cp_main.vl['Dat_BSI']['P103_Com_bRevGear']):
+      ret.gearShifter = GearShifter.reverse
     else:
       ret.gearShifter = GearShifter.drive
 
-    ret.stockFcw = False #cp_adas.vl['HS2_DYN_MDD_ETAT_2F6']['REQUEST_TAKEOVER'] == 2 # 0: no error, 1: non-critical request, 2: critical request
-    ret.stockAeb = False #bool(cp_adas.vl['HS2_DYN_MDD_ETAT_2F6']['AUTO_BRAKING_IN_PROGRESS'])
-
-    # button presses
+    # blinkers
     blinker = cp_main.vl['HS2_DAT7_BSI_612']['CDE_CLG_ET_HDC']
     ret.leftBlinker = blinker == 1
     ret.rightBlinker = blinker == 2
 
     # lock info
-    ret.doorOpen = False #any([cp_main.vl['Dat_BSI']['DRIVER_DOOR'], cp_main.vl['Dat_BSI']['PASSENGER_DOOR']])
+    ret.doorOpen = any([cp_main.vl['Dat_BSI']['DRIVER_DOOR'], cp_main.vl['Dat_BSI']['PASSENGER_DOOR']])
     ret.seatbeltUnlatched = cp_main.vl['RESTRAINTS']['DRIVER_SEATBELT'] != 2
     return ret
 
@@ -94,13 +90,11 @@ class CarState(CarStateBase):
       ('HS2_DYN_ABR_38D', 25),
       ('HS2_DYN_UCF_MDD_32D', 50),
       ('HS2_DAT_MDD_CMD_452', 20),
-      # ('HS2_DYN1_MDD_ETAT_2B6', 50),
-      # ('HS2_DYN_MDD_ETAT_2F6', 50),
+      ('HS2_DYN1_MDD_ETAT_2B6', 50),
     ]
     main_messages = [
       ('Dat_BSI', 20),
       ('RESTRAINTS', 10),
-      # ('DRIVER', 10),
       ('HS2_DAT7_BSI_612', 10),
     ]
     return {
