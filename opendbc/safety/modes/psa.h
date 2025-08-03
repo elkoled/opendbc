20e012a28c8043b6d3a0c6dc525e12118ec6f46b
elkoled <elkoled@gmail.com>
2025-08-03 15:40:06 +0200

    disable rx hook

--- opendbc/safety/modes/psa.h
+++ opendbc/safety/modes/psa.h
@@ -76,24 +76,24 @@ static void psa_rx_hook(const CANPacket_t *to_push) {
   int bus = GET_BUS(to_push);
   int addr = GET_ADDR(to_push);
 
-  if (bus == PSA_CAM_BUS) {
-    if (addr == PSA_DYN_CMM) {
-      gas_pressed = GET_BYTE(to_push, 3) > 0U; // P002_Com_rAPP
-    }
-    if (addr == PSA_STEERING) {
-      int torque_driver_new = to_signed(GET_BYTE(to_push, 1), 8);
-      update_sample(&torque_driver, torque_driver_new);
-    }
-    if (addr == PSA_STEERING_ALT) {
-      int angle_meas_new = to_signed((GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1), 16);
-      update_sample(&angle_meas, angle_meas_new);
-    }
-    if (addr == PSA_HS2_DYN_ABR_38D) {
-      int speed = (GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1);
-      vehicle_moving = speed > 0;
-      UPDATE_VEHICLE_SPEED(speed * 0.01); // VITESSE_VEHICULE_ROUES
-    }
-  }
+  // if (bus == PSA_CAM_BUS) {
+  //   if (addr == PSA_DYN_CMM) {
+  //     gas_pressed = GET_BYTE(to_push, 3) > 0U; // P002_Com_rAPP
+  //   }
+  //   if (addr == PSA_STEERING) {
+  //     int torque_driver_new = to_signed(GET_BYTE(to_push, 1), 8);
+  //     update_sample(&torque_driver, torque_driver_new);
+  //   }
+  //   if (addr == PSA_STEERING_ALT) {
+  //     int angle_meas_new = to_signed((GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1), 16);
+  //     update_sample(&angle_meas, angle_meas_new);
+  //   }
+  //   if (addr == PSA_HS2_DYN_ABR_38D) {
+  //     int speed = (GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1);
+  //     vehicle_moving = speed > 0;
+  //     UPDATE_VEHICLE_SPEED(speed * 0.01); // VITESSE_VEHICULE_ROUES
+  //   }
+  // }
 
   if (bus == PSA_ADAS_BUS) {
     if (addr == PSA_HS2_DAT_MDD_CMD_452) {
@@ -101,41 +101,44 @@ static void psa_rx_hook(const CANPacket_t *to_push) {
     }
   }
 
-  if (bus == PSA_MAIN_BUS) {
-    if (addr == PSA_DAT_BSI) {
-      brake_pressed = (GET_BYTE(to_push, 0U) >> 5U) & 1U; // P013_MainBrake
-    }
-  }
+  // if (bus == PSA_MAIN_BUS) {
+  //   if (addr == PSA_DAT_BSI) {
+  //     brake_pressed = (GET_BYTE(to_push, 0U) >> 5U) & 1U; // P013_MainBrake
+  //   }
+  // }
 }
 
 static bool psa_tx_hook(const CANPacket_t *to_send) {
-  bool tx = true;
-  int addr = GET_ADDR(to_send);
+  UNUSED(to_send);
+  return true;
 
-  static const AngleSteeringLimits PSA_STEERING_LIMITS = {
-      .angle_deg_to_can = 100,
-      .angle_rate_up_lookup = {
-      {0., 5., 25.},
-      {2.5, 1.5, 0.2},
-    },
-    .angle_rate_down_lookup = {
-      {0., 5., 25.},
-      {5., 2.0, 0.3},
-    },
-  };
+  // bool tx = true;
+  // int addr = GET_ADDR(to_send);
 
-  // Safety check for LKA
-  if (addr == PSA_LANE_KEEP_ASSIST) {
-    // SET_ANGLE
-    int desired_angle = to_signed((GET_BYTE(to_send, 6) << 6) | ((GET_BYTE(to_send, 7) & 0xFCU) >> 2), 14);
-    // TORQUE_FACTOR
-    bool lka_active = ((GET_BYTE(to_send, 5) & 0xFEU) >> 1) == 100U;
+  // static const AngleSteeringLimits PSA_STEERING_LIMITS = {
+  //     .angle_deg_to_can = 100,
+  //     .angle_rate_up_lookup = {
+  //     {0., 5., 25.},
+  //     {2.5, 1.5, 0.2},
+  //   },
+  //   .angle_rate_down_lookup = {
+  //     {0., 5., 25.},
+  //     {5., 2.0, 0.3},
+  //   },
+  // };
 
-    if (steer_angle_cmd_checks(desired_angle, lka_active, PSA_STEERING_LIMITS)) {
-      tx = false;
-    }
-  }
-  return tx;
+  // // Safety check for LKA
+  // if (addr == PSA_LANE_KEEP_ASSIST) {
+  //   // SET_ANGLE
+  //   int desired_angle = to_signed((GET_BYTE(to_send, 6) << 6) | ((GET_BYTE(to_send, 7) & 0xFCU) >> 2), 14);
+  //   // TORQUE_FACTOR
+  //   bool lka_active = ((GET_BYTE(to_send, 5) & 0xFEU) >> 1) == 100U;
+
+  //   if (steer_angle_cmd_checks(desired_angle, lka_active, PSA_STEERING_LIMITS)) {
+  //     tx = false;
+  //   }
+  // }
+  // return tx;
 }
 
 static bool psa_fwd_hook(int bus_num, int addr) {
@@ -151,7 +154,7 @@ static bool psa_fwd_hook(int bus_num, int addr) {
 static safety_config psa_init(uint16_t param) {
   UNUSED(param);
   static const CanMsg PSA_TX_MSGS[] = {
-    {PSA_LANE_KEEP_ASSIST, PSA_CAM_BUS, 8, .check_relay = true}, // EPS steering
+    {PSA_LANE_KEEP_ASSIST, PSA_CAM_BUS, 8, .check_relay = false}, // EPS steering
   };
 
   static RxCheck psa_rx_checks[] = {
