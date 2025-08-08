9d414c46a8cdf34398112f09c470b1723de1216f
elkoled <elkoled@gmail.com>
2025-08-08 15:52:36 +0200

    add psa-new state

---
+++ opendbc/car/psa/fingerprints.py
@@ -0,0 +1,15 @@
+from opendbc.car.structs import CarParams
+from opendbc.car.psa.values import CAR
+
+Ecu = CarParams.Ecu
+
+FW_VERSIONS = {
+  CAR.PSA_PEUGEOT_208: {
+    # ARTIV - Radar
+    (Ecu.fwdRadar, 0x6B6, None): [
+        b'212053276',
+        b'194504751',
+        b'222256113',
+    ],
+  }
+}
---
+++ opendbc/car/psa/values.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass, field
+
+from opendbc.car.structs import CarParams
+from opendbc.car import Bus, CarSpecs, DbcDict, PlatformConfig, Platforms
+from opendbc.car.lateral import AngleSteeringLimits
+from opendbc.car.docs_definitions import CarDocs, CarHarness, CarParts
+from opendbc.car.fw_query_definitions import FwQueryConfig, Request, uds
+
+Ecu = CarParams.Ecu
+
+
+class CarControllerParams:
+  STEER_STEP = 1
+
+  ANGLE_LIMITS: AngleSteeringLimits = AngleSteeringLimits(
+    390, # deg
+    ([0., 5., 25.], [2.5, 1.5, .2]),
+    ([0., 5., 25.], [5., 2., .3]),
+  )
+  STEER_DRIVER_ALLOWANCE = 5  # Driver intervention threshold, 0.5 Nm
+
+
+@dataclass
+class PSACarDocs(CarDocs):
+  package: str = "Adaptive Cruise Control (ACC) & Lane Assist"
+  car_parts: CarParts = field(default_factory=CarParts.common([CarHarness.psa_a]))
+
+
+@dataclass
+class PSAPlatformConfig(PlatformConfig):
+  dbc_dict: DbcDict = field(default_factory=lambda: {
+    Bus.pt: 'psa_aee2010_r3',
+  })
+
+
+class CAR(Platforms):
+  PSA_PEUGEOT_208 = PSAPlatformConfig(
+    [PSACarDocs("Peugeot 208 2019-25")],
+    CarSpecs(mass=1530, wheelbase=2.54, steerRatio=17.6),
+  )
+
+
+PSA_DIAG_REQ  = bytes([uds.SERVICE_TYPE.DIAGNOSTIC_SESSION_CONTROL, 0x01])
+PSA_DIAG_RESP = bytes([uds.SERVICE_TYPE.DIAGNOSTIC_SESSION_CONTROL + 0x40, 0x01])
+
+PSA_SERIAL_REQ = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER,  0xF1, 0x8C])
+PSA_SERIAL_RESP = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFIER + 0x40, 0xF1, 0x8C])
+
+PSA_RX_OFFSET = -0x20
+
+# PSA ECUs send an unpadded response on tester_present request.
+# _is_tester_present_response only accepts padded responses, so needed to be modified.
+FW_QUERY_CONFIG = FwQueryConfig(
+  requests=[
+    Request(
+      [PSA_DIAG_REQ, PSA_SERIAL_REQ],
+      [PSA_DIAG_RESP, PSA_SERIAL_RESP],
+      rx_offset=PSA_RX_OFFSET,
+      bus=1,
+      obd_multiplexing=False,
+    )
+  ]
+)
+
+DBC = CAR.create_dbc_map()
