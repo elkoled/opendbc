24ae1b590aa90b9ecfe7a2a167ba3d5b4bf85656
elkoled <elkoled@gmail.com>
2025-08-18 09:42:37 +0200

    add PSA_PEUGEOT_508

--- opendbc/car/psa/fingerprints.py
+++ opendbc/car/psa/fingerprints.py
@@ -10,7 +10,12 @@ FW_VERSIONS = {
         b'212053276', # Peugeot e208 Allure Pack
         b'194504751', # Peugeot e208 GT CC-only
         b'222256113', # Peugeot e208 GT NZ
+    ],
+  },
+  CAR.PSA_PEUGEOT_508: {
+    # ARTIV - Radar
+    (Ecu.fwdRadar, 0x6B6, None): [
         b'200603842', # Peugeot 508 Hybrid
     ],
-  }
+  },
 }
