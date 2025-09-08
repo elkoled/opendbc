2d98edbd7dd6b40453b8032a18657a620c972880
elkoled <elkoled@gmail.com>
2025-09-08 09:58:20 +0200

    PSA: add Peugeot 508

--- opendbc/car/psa/fingerprints.py
+++ opendbc/car/psa/fingerprints.py
@@ -1,4 +1,3 @@
-""" AUTO-FORMATTED USING opendbc/car/debug/format_fingerprints.py, EDIT STRUCTURE THERE."""
 from opendbc.car.structs import CarParams
 from opendbc.car.psa.values import CAR
 
@@ -6,8 +5,17 @@ Ecu = CarParams.Ecu
 
 FW_VERSIONS = {
   CAR.PSA_PEUGEOT_208: {
-    (Ecu.fwdRadar, 0x6b6, None): [
-      b'212053276',
+    # ARTIV - Radar
+    (Ecu.fwdRadar, 0x6B6, None): [
+        b'212053276', # Peugeot e208 Allure Pack
+        b'194504751', # Peugeot e208 GT CC-only
+        b'222256113', # Peugeot e208 GT NZ
+    ],
+  },
+  CAR.PSA_PEUGEOT_508: {
+    # ARTIV - Radar
+    (Ecu.fwdRadar, 0x6B6, None): [
+        b'200603842', # Peugeot 508 Hybrid
     ],
   },
 }
