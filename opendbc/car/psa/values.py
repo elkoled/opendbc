fdc1e28c3a9ae788e700bc944f1e1b3dda74446b
elkoled <elkoled@gmail.com>
2025-11-14 11:51:41 +0100

    revert steer driver allowance

--- opendbc/car/psa/values.py
+++ opendbc/car/psa/values.py
@@ -17,7 +17,7 @@ class CarControllerParams:
   STEER_DELTA_DOWN = 1  # TODO: torque decrease per refresh
   STEER_DRIVER_MULTIPLIER = 1  # TODO: weight driver torque
   STEER_DRIVER_FACTOR = 1
-  STEER_DRIVER_ALLOWANCE = 50  # TODO: tune Driver intervention threshold
+  STEER_DRIVER_ALLOWANCE = 5  # TODO: tune Driver intervention threshold
 
   def __init__(self, CP):
     pass
