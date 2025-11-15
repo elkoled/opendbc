f818d755d7b591ebcff29fdb48a381a4fbee5b8b
elkoled <elkoled@gmail.com>
2025-11-15 15:10:58 +0100

    add steer wheel hold prevention methods

--- opendbc/car/psa/carcontroller.py
+++ opendbc/car/psa/carcontroller.py
@@ -25,6 +25,7 @@ class CarController(CarControllerBase):
       new_torque = int(round(CC.actuators.torque * CarControllerParams.STEER_MAX))
       apply_torque = apply_driver_steer_torque_limits(new_torque, self.apply_torque_last,
                                                       CS.out.steeringTorque, CarControllerParams)
+      # TODO: test 2
       # emulate driver torque message at 1 Hz
       # if self.frame % 100 == 0:
       #   can_sends.append(create_driver_torque(self.packer, CS.steering))
@@ -38,8 +39,13 @@ class CarController(CarControllerBase):
     else:
       self.status = 4
 
+    # TODO: test 1
+    # if self.frame % 9000 == 0:
+    #   self.status = 2  # deactivate LKA every 9 seconds to avoid steering hold warning
+
     can_sends.append(create_lka_steering(self.packer, CC.latActive, apply_torque, self.status))
 
+    # TODO: test 3
     # if self.frame % 10 == 0:
     #   # send steering wheel hold message at 10 Hz to keep EPS engaged
     #   can_sends.append(create_steering_hold(self.packer, CC.latActive, CS.is_dat_dira))
