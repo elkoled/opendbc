235b50f7601faf171a0c7f6a87e737738e8298ea
elkoled <elkoled@gmail.com>
2025-12-14 16:12:10 +0100

    include drive in psacan

--- opendbc/car/psa/carcontroller.py
+++ opendbc/car/psa/carcontroller.py
@@ -70,11 +70,11 @@ class CarController(CarControllerBase):
       long_enabled = CC.longActive and CS.drive
 
       if self.frame % 2 == 0:
-        can_sends.append(create_HS2_DYN1_MDD_ETAT_2B6(self.packer, self.frame // 2, actuators.accel, long_enabled, CS.out.gasPressed, braking, CS.out.brakePressed, CS.out.standstill, torque))
+        can_sends.append(create_HS2_DYN1_MDD_ETAT_2B6(self.packer, self.frame // 2, actuators.accel, CS.out.cruiseState.enabled, CS.out.gasPressed, braking, CS.out.brakePressed, CS.out.standstill, CS.drive, torque))
         can_sends.append(create_HS2_DYN_MDD_ETAT_2F6(self.packer, braking, CC.hudControl.leadVisible))
 
       if self.frame % 10 == 0:
-        can_sends.append(create_HS2_DAT_ARTIV_V2_4F6(self.packer, long_enabled))
+        can_sends.append(create_HS2_DAT_ARTIV_V2_4F6(self.packer, CS.out.cruiseState.enabled))
 
       if self.frame % 100 == 0:
         can_sends.append(create_HS2_SUPV_ARTIV_796(self.packer))
