0090cf10e0be40eddca75c714fc0ba9dbddf3a20
elkoled <elkoled@gmail.com>
2026-01-14 19:46:11 -0800

    Opendbc replay (#253)
    * Revert "FORK TEST"
    
    This reverts commit fc691b500c58e76a41c8f5785b6acf89ff4e1573.
    
    * Revert "fix mutation test"
    
    This reverts commit 8f5e01de7743ee38c340df90751cf370634fe2b2.
    
    * fix comment
    
    * move car_diff.py
    
    * inline helpers
    
    * fix ruff
    
    * use all carstate fields
    
    * add --all
    
    * bootstrap
    
    * Revert "bootstrap"
    
    This reverts commit c3c2ba41e8d1dbdeb74cef8498caf9fd13325fe3.
    
    * add line formatting
    
    * commit workflow
    
    * adjust formatting
    
    * return err
    
    * subprocess
    
    * comma_car_segments
    
    * dead code
    
    * minimal logreader
    
    * remove openpilot
    
    * add deps
    
    * remove all deps
    
    * fix source
    
    * request
    
    * less parallel
    
    * fix push
    
    * bootstrap
    
    * Revert "bootstrap"
    
    This reverts commit b4af0d3bbd31f119ed3f18933329dcc64c9cf634.
    
    * fix graph
    
    * push
    
    * regex
    
    * optimize dict_diff
    
    * reduce tolerance
    
    * use comma_car_segments
    
    * use test pip package

--- opendbc/car/tests/car_diff.py
+++ opendbc/car/tests/car_diff.py
@@ -229,6 +229,48 @@ def logreader_from_url(url):
   return rlog.Event.read_multiple_bytes(data)
 
 
+def zstd_decompress(data):
+  proc = subprocess.run(['zstd', '-d'], input=data, capture_output=True, check=True)
+  return proc.stdout
+
+
+def zstd_compress(data):
+  proc = subprocess.run(['zstd', '-c'], input=data, capture_output=True, check=True)
+  return proc.stdout
+
+
+def dict_diff(d1, d2, path="", ignore=None, tolerance=0):
+  ignore = ignore or []
+  diffs = []
+  for key in d1.keys() | d2.keys():
+    if key in ignore:
+      continue
+    full_path = f"{path}.{key}" if path else key
+    v1, v2 = d1.get(key), d2.get(key)
+    if isinstance(v1, dict) and isinstance(v2, dict):
+      diffs.extend(dict_diff(v1, v2, full_path, ignore, tolerance))
+    elif isinstance(v1, (int, float)) and isinstance(v2, (int, float)):
+      if abs(v1 - v2) > tolerance:
+        diffs.append(("change", full_path, (v1, v2)))
+    elif v1 != v2:
+      diffs.append(("change", full_path, (v1, v2)))
+  return diffs
+
+
+def logreader_from_url(url):
+  import capnp
+
+  resp = requests.get(url)
+  assert resp.status_code == 200, f"Failed to download {url}: {resp.status_code}"
+  data = resp.content
+
+  if data.startswith(b'\x28\xB5\x2F\xFD'):  # zstd magic
+    data = zstd_decompress(data)
+
+  rlog = capnp.load(str(Path(__file__).parent / "rlog.capnp"))
+  return rlog.Event.read_multiple_bytes(data)
+
+
 def load_can_messages(seg):
   from opendbc.car.can_definitions import CanData
 
